be4ef61c1ae80548af94a174f66eefb17fc7b17a
romualdjunior <romuald.motchehokamguia@esprit.tn>
2023-01-18 20:22:20 +0100

    solving deployment issue

--- main/settings.py
+++ main/settings.py
@@ -221,7 +221,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # # Heroku setting
 # # Serving the statics through Whitenoise in DigitalOcean
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
